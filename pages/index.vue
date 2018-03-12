44b0c24f02de4ef7497ea42e3e43e499da742206
Kaplaugher <kaplaugher94@gmail.com>
2018-03-12 06:53:30 -0400

    init

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Hello</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.heroimg {
+  height: 40vh;
+  width: 100%;
+  background-color: aquamarine;
+}
+
+</style>
